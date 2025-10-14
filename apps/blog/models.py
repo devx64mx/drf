65eacace974f9fcc28bde50764c8def54376f4a0
Erne <siriusx711@gmail.com>
2025-10-13 19:36:21 -0600

    add Models Category and Post

--- apps/blog/models.py
+++ apps/blog/models.py
@@ -1,3 +1,64 @@
-from django.db import models
+import uuid
 
-# Create your models here.
+from django.db import models
+from django.utils import timezone
+
+def blog_thumbnail_directory(instance, filename):
+    return "blog/{0}/{1}".format(instance.title, filename)
+
+def category_thumbnail_directory(instance, filename):
+    return "blog_categories/{0}/{1}".format(instance.name, filename)
+
+class Category(models.Model):
+
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    parent = models.ForeignKey("self", related_name="children", on_delete=models.CASCADE, blank=True, null=True)
+
+    name = models.CharField(max_length=255)
+    title = models.CharField(max_length=255, blank=True, null=True)
+    description = models.TextField()
+    thumbnail = models.ImageField(upload_to=category_thumbnail_directory)
+    slug = models.CharField(max_length=128)
+
+    def __str__(self):
+        return self.name
+
+
+class Post(models.Model):
+
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+
+    class PostObjects(models.Manager):
+        def query_set(self):
+            return super().get_queryset().filter('published')
+
+    status_options = (
+        ("draft","Draft"),
+        ("published","Published")
+    )
+
+    title = models.CharField(max_length=128)
+    description = models.CharField(max_length=256)
+    content = models.TextField()
+    thumbnail = models.ImageField(upload_to=blog_thumbnail_directory)
+
+    keywords = models.CharField(max_length=128)
+    slug = models.CharField(max_length=128)
+
+    category = models.ForeignKey(Category, on_delete=models.PROTECT)
+    
+    created_at = models.DateTimeField(default=timezone.now)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    status = models.CharField(max_length=10, choices=status_options, default='draft')
+
+    object = models.Manager() # default Manager
+    postobjects = PostObjects() # custom Manager
+
+    class Meta:
+        ordering = ("-published")
+    
+    def __str__(self):
+        return self.title
+
+
--- core/urls.py
+++ core/urls.py
@@ -6,6 +6,6 @@ from django.conf import settings # type: ignore
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT )
 
 
